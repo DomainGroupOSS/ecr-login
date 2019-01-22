1f2d56fca37106d952337a54e3bf2894cb96d26c
cheney-yan <cheney.yan@domain.com.au>
2019-01-22 17:08:20 +1100

    Update ecr-login.sh
    REGISTRY_IDS is really optional

--- ecr-login.sh
+++ ecr-login.sh
@@ -2,7 +2,11 @@
 
 while true; do
 	date
-	aws ecr --region=$AWS_REGION get-login --registry-ids REGISTRY_IDS | /bin/sh
+	if [[ "" = "$REGISTRY_IDS" ]]' then
+		aws ecr --region=$AWS_REGION get-login | /bin/sh
+	else
+		aws ecr --region=$AWS_REGION get-login --registry-ids REGISTRY_IDS | /bin/sh
+	fi
 	sleep 500
 done
 
